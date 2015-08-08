0d8927f5e6356b833657dab7f2880a6050a8f2d8
Quan Yu <quanyu@205-135.priv23.nus.edu.sg>
2015-08-08 16:07:06 +0800

    2nd changes made

--- football.r
+++ football.r
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(igraph)
+library(networkD3)
 aa = read.graph("http://networkdata.ics.uci.edu/data/football/football.gml", format = "gml")
 conf = data.frame(id = 0:11, group = c(
   "Atlantic Coast",
@@ -17,7 +18,8 @@ conf = data.frame(id = 0:11, group = c(
   "Western Athletic"
 ))
 
-d.nodes = data.frame(name = V(aa)$label, id = V(aa)$value)
+#d.nodes = data.frame(name = V(aa)$label, id = V(aa)$value, degree = degree(aa) * 5)
+d.nodes = data.frame(name = V(aa)$label, id = V(aa)$value, degree = degree(aa) * 2 - 12)
 d.nodes$size = 8
 print(head(d.nodes))
 print(tail(d.nodes))
@@ -28,13 +30,12 @@ print(tail(d.nodes))
 d.edges = as.data.frame(get.edgelist(aa))
 d.edges$V1 = d.edges$V1 - 1; d.edges$V2 = d.edges$V2 - 1
 colnames(d.edges) = c("source", "target")
-d.edges$value = 2
+d.edges$value = 1
 
 
 q()
-library(networkD3)
 forceNetwork(Links = d.edges, Nodes = d.nodes,
             Source = "source", Target = "target",
             NodeID = "name", Value = "value", 
-	    #  Nodesize = 8, 
+	    Nodesize = "degree", 
             Group = "group", opacity = 0.9, legend = TRUE)
